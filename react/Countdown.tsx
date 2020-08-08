5481021f335fc177fc414ba80106b0f1bfc004b5
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-08-08 10:51:10 -0300

    reating the countdown block feature

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TimeSplit } from './typings/global';
+import { tick } from './utils/time';
 
 interface CountdownProps { 
   targetDate: string;
 }
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
+const DEFAULT_TARGET_DATE = (new Date('2020-08-08')).toISOString()
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
+  tick(targetDate, setTime)
+
   return (<div>
     <h1>{ targetDate }</h1>
+    <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
   </div>)
 }
 
