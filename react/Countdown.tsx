0678d6026d016905ba36e4cbcccba7ff6e16af75
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-08-08 11:07:37 -0300

    modifying the countdown block to have configurable styles

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
 import { TimeSplit } from './typings/global';
 import { tick } from './utils/time';
+import { useCssHandles } from "vtex.css-handles";
 
 interface CountdownProps { 
   targetDate: string;
 }
 
-const DEFAULT_TARGET_DATE = (new Date('2020-08-08')).toISOString()
+const DEFAULT_TARGET_DATE = (new Date('2020-08-08')).toISOString();
+const CSS_HANDLES = ["countdown"];
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
 
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
-    seconds: '00'
+    seconds: '00',
   })
+
+  const handles = useCssHandles(CSS_HANDLES);
 
   tick(targetDate, setTime)
 
-  return (<div>
-    <h1>{ targetDate }</h1>
-    <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
-  </div>)
+  return (
+    <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
+      <h1>{ targetDate }</h1>
+      <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
+    </div>
+  )
 }
 
 Countdown.schema = {
